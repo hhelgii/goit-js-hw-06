ade5738e1ddd05914f61769d6d6463c65d556314
hhelgii <opruska2004@gmail.com>
2023-04-22 13:03:14 +0300

    update

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,16 @@
-const categories=document.getElementById('categories');
-const categoriesList=categories.children;
-console.log(`Number of categories : ${categoriesList.length}`);
-const subCategories=categories.querySelectorAll('.item');
+// const categories=document.getElementById('categories');
+// const categoriesList=categories.children;
+// console.log(`Number of categories : ${categoriesList.length}`);
+// const subCategories=categories.querySelectorAll('.item');
 
-subCategories.forEach(item => {
-    const title=item.querySelector('h2').textContent;
-    const elements=item.querySelectorAll('li').length;
-    console.log(`\nCategory: ${title} \nElements: ${elements}`);
-});
+// subCategories.forEach(item => {
+//     const title=item.querySelector('h2').textContent;
+//     const elements=item.querySelectorAll('li').length;
+//     console.log(`\nCategory: ${title} \nElements: ${elements}`);
+// });
+const items = document.querySelectorAll(".item");
+console.log(`Number of categories : ${items.length}`);
+items.forEach(item=>{
+    console.log(`Category ${item.firstElementChild.textContent}`);
+    console.log(`Elements ${item.lasttElementChild.children.length}`);
+})
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
-const text=document.getElementById('text');
-const input=document.getElementById('font-size-control');
-input.addEventListener('input', changeTextSize);
+const text = document.getElementById("text");
+const input = document.getElementById("font-size-control");
+text.style.fontSize = input.value + "px";
+input.addEventListener("input", changeTextSize);
 function changeTextSize(event) {
-    text.style.fontSize=event.target.value+'px';
+  text.style.fontSize = event.target.value + "px";
 }
